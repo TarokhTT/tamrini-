c034b7ebdb4499394680f8663346fcc657053960
Ehsan Safdari <essafdari99@gmail.com>
2026-08-11 12:23:22 +0330

    the second update for login

--- login.py
+++ login.py
@@ -2,3 +2,7 @@ print("welcome to login")
 print("the login feature is updated")
 print("hi this message is for test")
 print("this is ehsan")
+a = true
+if(a){
+    print("this is the print line that added for the last time")
+}
